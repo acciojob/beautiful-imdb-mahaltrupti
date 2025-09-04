93629e85a96db01d6197b26b369fa1171e4423a0
mahaltrupti <truptimahal2004@gmail.com>
2025-09-04 11:22:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //your code here
- <button class="btn" onclick="changeMovie()">Change Movie</button>
 
-  <script>
     function changeMovie() {
       // Example: dynamically change details
       document.querySelector("h1").textContent = "The Dark Knight (2008)";
@@ -9,4 +7,4 @@
       document.querySelector(".movieDetail").textContent =
         "Batman faces the Joker, a criminal mastermind who plunges Gotham City into chaos. Directed by Christopher Nolan, starring Christian Bale and Heath Ledger.";
     }
-  </script>
+
